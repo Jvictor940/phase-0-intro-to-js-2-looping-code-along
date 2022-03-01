466d244dfe07d1a7deb838372d67051c985d953c
Jerry <Jvictor940@gmail.com>
2022-03-01 04:31:33 -0500

    commit tree a8a3078d619c5a1fefc0ca594de4a719b6f5208c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Joe", "Nina", "Jay"]
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "birthday"))
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+    return countDown
+}
+countDown()
